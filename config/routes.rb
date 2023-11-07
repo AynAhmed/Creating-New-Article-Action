782068c3d7d0c493ee39a436280f87d503f07205
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2023-11-07 16:10:31 -0600

    class practice lesson

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-Rails.application.routes.draw do 
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,10 +9,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 
-  Rails.application.routes.draw do
-    root "articles#index"
-  
-    resources :articles
+  root 'articles#index'
+
+  resources :articles do
+    resources :comments
   end
-  
 end
