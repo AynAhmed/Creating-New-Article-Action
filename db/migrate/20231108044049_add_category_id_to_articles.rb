77dad1caa13b789a0d0a67dab1816e0855c74a71
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2023-11-08 11:43:28 -0600

    small fixes

--- db/migrate/20231108044049_add_category_id_to_articles.rb
+++ db/migrate/20231108044049_add_category_id_to_articles.rb
@@ -1,6 +1,5 @@
 class AddCategoryIdToArticles < ActiveRecord::Migration[6.0]
   def change
-    add_reference :articles, :category, foreign_key: true, null: true
-  end
+    add_reference :articles, :category, foreign_key: true
 end
 
